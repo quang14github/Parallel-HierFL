0151dc8ddc99f1a47c5c639b560e3033f482194d
quangdm <qduongminh3@gmail.com>
2024-12-28 21:39:20 +0700

    dql ucb1 quantity

--- options.py
+++ options.py
@@ -67,7 +67,7 @@ def args_parser():
     parser.add_argument(
         "--iid",
         type=int,
-        default=1,
+        default=0,
         help="distribution of the data among clients, 0 (non-iid), 1 (iid)",
     )
     parser.add_argument(
